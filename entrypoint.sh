a7c0a21407fe51f679ca4956b2f1b955ce4c9d23
Sam <sam@thesamgordon.com>
2025-12-06 08:27:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -z "$PATRONI1_IP" ] || [ -z "$PATRONI2_IP" ] || [ -z "$PATRONI3_IP" ]; then
-    echo "Error: PATRONI1_IP, PATRONI2_IP, and PATRONI3_IP must be set"
+if [ -z "$PATRONI1_IP" ] || [ -z "$PATRONI2_IP" ] || [ -z "$PATRONI3_IP" ] || [ -z "$STATS_PASSWORD" ]; then
+    echo "Error: PATRONI1_IP, PATRONI2_IP, PATRONI3_IP, and STATS_PASSWORD must be set"
     exit 1
 fi
 
@@ -11,6 +11,6 @@ precedence ::ffff:0:0/96  10
 precedence ::/0           100
 EOF
 
-envsubst '${PATRONI1_IP} ${PATRONI2_IP} ${PATRONI3_IP}' < /usr/local/etc/haproxy/haproxy.template.cfg > /usr/local/etc/haproxy/haproxy.cfg
+envsubst '${PATRONI1_IP} ${PATRONI2_IP} ${PATRONI3_IP} ${STATS_PASSWORD}' < /usr/local/etc/haproxy/haproxy.template.cfg > /usr/local/etc/haproxy/haproxy.cfg
 
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
