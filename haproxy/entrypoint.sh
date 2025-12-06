652bd0b319e71c83c168461d1fc6a17f51983c07
Sam <sam@thesamgordon.com>
2025-12-05 23:03:25 -0500

    Update entrypoint.sh

--- haproxy/entrypoint.sh
+++ haproxy/entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ -z "$PATRONI_HOST_1" ] || [ -z "$PATRONI_HOST_2" ] || [ -z "$PATRONI_HOST_3" ]; then
-    echo "Error: PATRONI_HOST_1, PATRONI_HOST_2, and PATRONI_HOST_3 must be set"
+    echo "Error: PATRONI1_IP, PATRONI2_IP, and PATRONI3_IP must be set"
     exit 1
 fi
 
-envsubst '${PATRONI_HOST_1} ${PATRONI_HOST_2} ${PATRONI_HOST_3}' < /usr/local/etc/haproxy/haproxy.template.cfg > /usr/local/etc/haproxy/haproxy.cfg
+envsubst '${PATRONI1_IP} ${PATRONI2_IP} ${PATRONI3_IP}' < /usr/local/etc/haproxy/haproxy.template.cfg > /usr/local/etc/haproxy/haproxy.cfg
 
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
